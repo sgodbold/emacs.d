db22aa4c36e8a1193ec0b234b35ce8fb1d836c32
sgodbold <steven.godbold@gmail.com>
2026-02-28 21:43:13 -0500

    update init-go

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # emacs.d
+
+## install
+
+golang lsp (make sure it's on your PATH)
+- go install golang.org/x/tools/gopls@latest
+- go install golang.org/x/tools/cmd/goimports@latest
+
+debugger
+- go install github.com/go-delve/delve/cmd/dlv@latest
--- lisp/init-go.el
+++ lisp/init-go.el
@@ -3,7 +3,42 @@
 ;;; Code:
 
 (when (maybe-require-package 'go-mode)
-  (maybe-require-package 'smarty-mode))
+
+  ;; Use goimports instead of gofmt
+  (setq gofmt-command "goimports")
+
+  ;; Format on save
+  ;; (add-hook 'before-save-hook #'gofmt-before-save)
+  (add-hook 'go-mode-hook
+            (lambda ()
+              (add-hook 'before-save-hook #'gofmt-before-save nil t)))
+
+  ;; Organize imports on save
+  (setq lsp-go-format-tool "goimports")
+
+  ;; LSP setup
+  (add-hook 'go-mode-hook #'eglot-ensure)
+
+  ;; Optional: flycheck integration
+  (when (maybe-require-package 'flycheck)
+    (add-hook 'go-mode-hook #'flycheck-mode))
+
+  ;; go requires tabs not spaces
+  (setq tab-width 4)
+  (setq indent-tabs-mode t)
+
+  (add-hook 'go-mode-hook
+            (lambda ()
+              (evil-define-key 'normal (current-local-map)
+                (kbd "M-.") #'xref-find-definitions
+                (kbd "M-?") #'xref-find-references)))
+
+  (use-package dap-mode
+    :after go-mode
+    :config
+    (dap-auto-configure-mode)
+    (require 'dap-dlv-go))
+  )
 
 (provide 'init-go)
 ;;; init-go.el ends here
